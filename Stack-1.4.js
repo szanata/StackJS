bd70af59a438683f3093a31a1d025b669bbb5b60
unknown <made.in.stefano@gmail.com>
2012-08-07 01:21:33 -0300

    1.4

---
+++ Stack-1.4.js
@@ -0,0 +1,213 @@
+﻿/**
+* StackJS 1.4 - now faster
+*    
+* <see entire project at http://github.com/madeinstefano/StackJS>
+*
+* @author Stéfano Stypulkowski <http://szanata.com>
+*     
+* @license GPL <http://szanata.com/license/GPL.txt>
+* @license MIT <http://szanata.conm/license/MIT.txt>
+*/
+
+//Array indexOf implementation
+if (!Array.prototype.indexOf){
+
+  Array.prototype.indexOf = function (elt){
+  
+    var
+      len = this.length >>> 0,
+      from = Number(arguments[1]) || 0;
+      from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+      
+    if (from < 0){
+      from += len;
+    }
+
+    for (; from < len; from++){
+      if (from in this && this[from] === elt) {
+        return from;
+      }
+    }
+    return -1;
+  };
+}
+
+//Array clone implementation
+if (!Array.prototype.clone){
+  Array.prototype.clone = function (){
+  
+    var a = [];
+    for (var i = 0, li = this.length; i < li; i++){
+      a.push(this[i]);
+    }
+    return a;
+  }
+}
+
+var Stack = window.Stack = {};
+
+(function (){
+  'use strict';
+  
+  var
+    _stack = [],
+    _map = [],
+    _states = [];
+    
+
+  function isUniqueId(id){
+    return _map.indexOf(id) === -1;
+  }
+
+  var StackItem = function StackItem(id, obj){
+    this.id = id;
+    this.obj = obj;
+  }
+  StackItem.prototype.constructor = StackItem;
+
+  var StackState = function StackState(id,stack,map){
+    this.id = id;
+    this.stack = stack;
+    this.map = map;
+  }
+  StackState.prototype.constructor = StackState;
+
+  /*
+  * puts some item in the stack
+  */
+  function put(obj, id, overwrite){
+    
+    if (!isUniqueId(id) && overwrite === false){
+      return false;
+    }
+    
+    var 
+      item = new StackItem(id,obj),
+      pos = _map.indexOf(id);
+    
+    if ( pos > -1){
+      _stack[pos] = item;
+    }else{
+      _map[_stack.push(item) - 1] = id;
+    }
+    return true;
+  }
+  Stack['put'] = put;
+  
+  /**
+  * returns some item in the stack by given id
+  */
+  function get(id){
+    var pos = _map.indexOf(id);
+    return pos >= 0 ? _stack[pos].obj : null;
+  }
+  Stack['get'] = get;
+  
+  /**
+  * save a state of the stack
+  */
+  function save(label){
+    if (label !== undefined && typeof label !== 'number' && typeof label !== 'string'){
+      throw 'label must be omitted or a Number or a String.'
+    }
+    if (label !== undefined){
+      var i = _states.length;
+      while (i--){
+        if (_states[i].id === label){
+          _states[i] = new StackState(label,_stack.clone(),_map.clone());
+          return;
+        }
+      }
+    }
+    _states.push(new StackState(label,_stack.clone(),_map.clone()));
+  }
+  Stack['save'] = save;
+  
+  /**
+  * restore a previous saved state of the stack
+  */
+  function restore(label){
+    if (label !== undefined && typeof label !== 'number' && typeof label !== 'string'){
+      throw 'label must be omitted or a Number or a String.'
+    }
+    if (label === undefined && _states.length > 0){
+      _stack = _states[_states.length - 1].stack;
+      _map = _states[_states.length - 1].map;
+      _states.pop();
+      return true;
+    }else{
+      var i = _states.length;
+      while (i--){
+        if (_states[i].id == label){
+          _stack = _states[i].stack;
+          _map = _states[i].map;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  Stack['restore'] = restore;
+  
+  /**
+  * exile some element from the stack and return it
+  */
+  function exile(id){
+    var pos = _map.indexOf(id);
+    if (pos >= 0){
+      var item = _stack[pos].obj; 
+      _map.splice(pos,1);
+      _stack.splice(pos,1);
+      return item;
+    }
+    return null;
+  }
+  Stack['exile'] = exile;
+      
+  /**
+  * removes items from the stack by their ids
+  * returns the amount of removed items
+  */
+  function remove(){
+    var removed = 0;
+    
+    for (var i = 0, l = arguments.length; i < l;i++){
+          
+      var pos = _map.indexOf(arguments[i]);
+      
+      if (pos >= 0){
+        var item = _stack[pos].obj; 
+        _map.splice(pos,1);
+        _stack.splice(pos,1);
+        removed++;
+      }
+    }
+    return removed;
+  }
+  Stack['remove'] = remove;
+
+
+  /**
+  * clear the stack, but not the states
+  */
+  function clear(){
+    _stack.length = _map.length = 0;
+  }
+  Stack['clear'] = clear;
+      
+  /**
+  * reset the stack, including the saved states
+  */
+  function reset(){
+    _stack.length = _map.length = _states.length = 0;
+  }
+  Stack['reset'] = reset;
+
+  /**
+  * returns the amount of items in the Stack
+  */
+  function size(){
+    return _stack.length;
+  }
+  Stack['size'] = size;
+})();
--- tests/unitary-tests.js
+++ tests/unitary-tests.js
@@ -1,3 +1,4 @@
+
 module('StackJS');
 
 test('JS Integrity', function (){
@@ -22,157 +23,188 @@ test('Array.prototype.clone', function (){
   
 });
 
-test('simple set/get data', function (){
-    var c1 = {};
-    c1.model = 'calibra';
-    c1.year = 95;
-    c1.manufacter = 'Opel';
+test('storing different data types', function (){
     
-    var c2 = {};
-    c2.model = '535i';
-    c2.year = 97;
-    c2.manufacter = 'BMW';
-    
-    ok(Stack.put(c1,'my first car'),'Adding something');
-    ok(Stack.put(c2,'my second car'),'Adding one more thing');
-    
-    equals(Stack.get('a car that I never had'),null,'Getting nothing');
-    equals(Stack.get('my first car'),c1,'Getting the first thing');
-    equals(Stack.get('my second car'),c2,'Getting the second thing');
+  var 
+    string = '',
+    number = 0,
+    date = new Date(),
+    regex = /./g,
+    func = function (){},
+    object = {},
+    buildin = HTMLElement,
+    bool = true,
+    und = undefined,
+    nil = null;
+
+  ok(Stack.put(string,'string'),'Storing a String');
+  ok(Stack.put(number,'number'),'Storing a Number');
+  ok(Stack.put(bool,'boolean'),'Storing a Boolean');
+  ok(Stack.put(und,'undefined'),'Storing a undefined');
+  ok(Stack.put(nil,'null'),'Storing a null');
+  ok(Stack.put(date,'date'),'Storing a Date');
+  ok(Stack.put(regex,'regex'),'Storing a Regex');
+  ok(Stack.put(func,'function'),'Storing a Function');
+  ok(Stack.put(object,'object'),'Storing a Object');
+  ok(Stack.put(buildin,'HTMLElement'),'Storing a HTMLElement');
+  
+  equals(Stack.get('string'),string,'Getting the String');
+  equals(Stack.get('number'),number,'Getting the Number');
+  equals(Stack.get('boolean'),bool,'Getting the Boolean');
+  equals(Stack.get('undefined'),und,'Getting the undefined');
+  equals(Stack.get('null'),nil,'Getting the null');
+  equals(Stack.get('date'),date,'Getting the Date');
+  equals(Stack.get('regex'),regex,'Getting the Regex');
+  equals(Stack.get('function'),func,'Getting the Function');
+  equals(Stack.get('object'),object,'Getting the Object');
+  equals(Stack.get('HTMLElement'),buildin,'Getting the HTMLElement');
+  Stack.clear();
 });
+
+test('storing with diffrente types of ids', function (){
+  var 
+    string = '',
+    number = 0,
+    date = new Date(),
+    regex = /./g,
+    func = function (){},
+    object = {},
+    buildin = HTMLElement,
+    bool = true,
+    und = undefined,
+    nil = null;
+
+  ok(Stack.put(0,string),'Storing with a String id');
+  ok(Stack.put(1,number),'Storing with a Number id');
+  ok(Stack.put(2,bool),'Storing with a Boolean id');
+  ok(Stack.put(3,und),'Storing with a undefined id');
+  ok(Stack.put(4,nil),'Storing with a null id');
+  ok(Stack.put(5,date),'Storing with a Date id');
+  ok(Stack.put(6,regex),'Storing with a Regex id');
+  ok(Stack.put(7,func),'Storing with a Function id');
+  ok(Stack.put(8,object),'Storing with a Object id');
+  ok(Stack.put(9,buildin),'Storing with a HTMLElement id');
+
+  equals(Stack.get(string),0,'Getting the String id');
+  equals(Stack.get(number),1,'Getting the Number id');
+  equals(Stack.get(bool),2,'Getting the Boolean id');
+  equals(Stack.get(und),3,'Getting the undefined id');
+  equals(Stack.get(nil),4,'Getting the null id');
+  equals(Stack.get(date),5,'Getting the Date id');
+  equals(Stack.get(regex),6,'Getting the Regex id');
+  equals(Stack.get(func),7,'Getting the Function id');
+  equals(Stack.get(object),8,'Getting the Object id');
+  equals(Stack.get(buildin),9,'Getting the HTMLElement id');
+  Stack.clear();
+});
+
 test('update data', function (){
     
-    Stack.put(1,'my secret number');
-    
-    equals(Stack.get('my secret number'),1,'The secret number was stored');
-    
-    Stack.put(2,'my secret number');
-    
-    equals(Stack.get('my secret number'),2,'The secret number was updated');
-    
-    Stack.put(3,'my secret number',true);
-    
-    equals(Stack.get('my secret number'),2,'The secret number wasn\'t update because I said NOOVERWRITE');
-
+  Stack.put(1,'my secret number');
+  equals(Stack.get('my secret number'),1,'The secret number was stored');
+  Stack.put(2,'my secret number');
+  equals(Stack.get('my secret number'),2,'The secret number was updated');
+  Stack.put(3,'my secret number',false);
+  equals(Stack.get('my secret number'),2,'The secret number wasn\'t update because the thrid parameter was specified');
+  Stack.clear();
 });
 
-test('deleteing data', function (){
+test('deleting data', function (){
 
-    Stack.put(Math.PI,'PI');
-    
-    equals(Stack.get('PI'),Math.PI,'PI is safe');
-    equals(Stack.exile('PI'),Math.PI,'PI is now being removed');
-    equals(Stack.exile('PI'),null,'PI is gone');
-    
-    for (var i = 0; i < 5 ; i++){
-        Stack.put(i,String(i));
-    }
-    var total = Stack.get('0') + Stack.get('1') + Stack.get('2');
-    equals(total,3,'Added 3 fuckin\' pigs inda house');
-    
-    equals(Stack.remove('0','1','2'),3,'Da wolf is removing da funckin pigs');
-    
-    total = String(Stack.get('0')) + String(Stack.get('1')) + String(Stack.get('2'));
-    equals(total,'nullnullnull','Da funckin\' pigs are dead');
+  Stack.put(Math.PI,'PI');
+  
+  equals(Stack.get('PI'),Math.PI,'PI is safe');
+  equals(Stack.exile('PI'),Math.PI,'PI is now being removed');
+  equals(Stack.exile('PI'),null,'PI is gone');
+  
+  for (var i = 0; i < 3 ; i++){
+    Stack.put(i,i);
+  }
+  var total = Stack.get(0) + Stack.get(1) + Stack.get(2);
+  equals(total,3,'Added 3 elements to Stack');
+  
+  equals(Stack.remove(0,1,2),3,'Removing 3 elements at the same time');
+  
+  total = String(Stack.get(0)) + String(Stack.get(1)) + String(Stack.get(2));
+  equals(total,'nullnullnull','All elements was removed');
+  Stack.clear();
 });
 
 test('clear and size', function (){
 
-    Stack.clear();
-    equals(Stack.size(),0,'The stack size is 0! [EMPTY]');
-    
-    Stack.put(Math.PI,'PI');
-    Stack.put(Math.E,'E');
-    
-    equals(Stack.size(),2,'The stack size has now 2 items!');
-    
-    Stack.clear();
-    equals(Stack.size(),0,'The stack is clear again');
+  Stack.clear();
+  equals(Stack.size(),0,'The stack size is 0! [EMPTY]');
+  
+  Stack.put(Math.PI,'PI');
+  Stack.put(Math.E,'E');
+  
+  equals(Stack.size(),2,'The stack size has now 2 items!');
+  
+  Stack.clear();
+  equals(Stack.size(),0,'The stack is clear again');
 });
 
-test('saving point without label and restore it without parameters', function (){
+test('saving point without label and restore it without parameters (last)', function (){
 
-    Stack.reset();
-    
-    Stack.put(1,'A1');
-    Stack.put(2,'A2');
-    Stack.put(3,'A3');
-    
-    Stack.save();
-    
-    Stack.put(4,'A4');
-    Stack.put(5,'A5');
-    
-    Stack.save();
-    
-    Stack.put(6,'A6');
-    Stack.put(7,'A7');
-    
-    ok(Stack.restore(),'Must restore the last save point');
-    equals(Stack.size(),5,'The last point is now the current');
-});
-
-test('saving point without label and restore it by its number', function (){
-
-    Stack.reset();
-    
-    Stack.put(1,'A1');
-    Stack.put(2,'A2');
-    Stack.put(3,'A3');
-    
-    Stack.save();
-    
-    Stack.put(4,'A4');
-    Stack.put(5,'A5');
-    
-    Stack.save();
-    
-    Stack.put(6,'A6');
-    Stack.put(7,'A7');
-    
-    ok(Stack.restore(0),'Must restore the first save point, which is 0');
-    equals(Stack.size(),3,'The first save point is now the current');
+  Stack.reset();
+  
+  Stack.put(1,'A1');
+  Stack.put(2,'A2');
+  Stack.put(3,'A3');
+  
+  Stack.save();
+  
+  Stack.put(4,'A4');
+  Stack.put(5,'A5');
+  
+  Stack.save();
+  
+  Stack.put(6,'A6');
+  Stack.put(7,'A7');
+  
+  ok(Stack.restore(),'Must restore the last save point');
+  equals(Stack.size(),5,'The last point is now the current');
 });
 
 test('saving points with label and restoring them', function (){
 
-    Stack.reset();
-    
-    Stack.put(1,'A1');
-    Stack.put(2,'A2');
-    Stack.put(3,'A3');
-    
-    Stack.save('first point');
-    
-    Stack.put(4,'A4');
-    Stack.put(5,'A5');
-    
-    Stack.save('second point');
-    
-    Stack.put(6,'A6');
-    Stack.put(7,'A7');
-    
-    ok(Stack.restore('second point'),'Must restore the second save point');
-    equals(Stack.size(),5,'The second save point is now official');
-    
-    ok(Stack.restore('first point'),'Must restore the first save point');
-    equals(Stack.size(),3,'The first save point is now official');
+  Stack.reset();
+  
+  Stack.put(1,'A1');
+  Stack.put(2,'A2');
+  Stack.put(3,'A3');
+  
+  Stack.save('first point');
+  
+  Stack.put(4,'A4');
+  Stack.put(5,'A5');
+  
+  Stack.save('second point');
+  
+  Stack.put(6,'A6');
+  Stack.put(7,'A7');
+  
+  ok(Stack.restore('second point'),'Must restore the second save point');
+  equals(Stack.size(),5,'The second save point is now official');
+  
+  ok(Stack.restore('first point'),'Must restore the first save point');
+  equals(Stack.size(),3,'The first save point is now official');
 });
 
 test('Reseting', function (){
 
-    Stack.put(1,'A1');
-    Stack.put(2,'A2');
-    Stack.put(3,'A3');
-    
-    Stack.save('check point');
-    
-    Stack.put(1,'A1');
-    Stack.put(2,'A2');
-    Stack.put(3,'A3');
-    
-    Stack.reset();
-    
-    ok(!Stack.restore('check point'),'Nothing to restore');
-    equals(Stack.size('check point'),0,'The stack is empty');
+  Stack.put(1,'A1');
+  Stack.put(2,'A2');
+  Stack.put(3,'A3');
+  
+  Stack.save('check point');
+  
+  Stack.put(1,'A1');
+  Stack.put(2,'A2');
+  Stack.put(3,'A3');
+  
+  Stack.reset();
+  
+  ok(!Stack.restore('check point'),'Nothing to restore');
+  equals(Stack.size('check point'),0,'The stack is empty');
 });
